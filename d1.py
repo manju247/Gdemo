d84415512ab35fd98413ce6f60f6c0e2e8e6f217
manju <manjunathshastri247@gmail.com>
2020-09-17 01:03:25 +0530

    first commit

--- d1.py
+++ d1.py
@@ -5,4 +5,10 @@ print(lst)
 print(type(lst))
 print(len(lst))
 print(id(lst))
-lst2 = [1,3,6,2]
+lst2 = [1,3,8,2,9]
+l = len(lst)
+l1 = len(lst2)
+for i in lst:
+    if i == 23.50:
+        print("Value is same")
+        break 
